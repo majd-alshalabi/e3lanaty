9a00ac85a3322764397de31bce01434a3873e74f
majd211 <alshalabi211@gmail.com>
2023-07-11 17:34:04 +0300

    commit

--- app/Http/Controllers/Ads/SearchController.php
+++ app/Http/Controllers/Ads/SearchController.php
@@ -11,6 +11,7 @@ use App\Models\Like;
 use App\Models\User;
 use App\response_trait\MyResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class SearchController extends Controller
@@ -18,126 +19,54 @@ class SearchController extends Controller
     use MyResponseTrait;
 
     public function searchForAds(Request $request)
-    {  
-        if ($request->search_word != null && $request->type != null) {
-            $ads = Ads::where('name' ,'like' ,$request->search_word)->andWhere('type',$request->type)->orderBy('created_at', 'desc')->with('advantages')
-                ->with('images')
-                ->paginate(Constant::NUM_OF_PAGE)
-            ;
-            $currentUser = $request->user();
-            foreach ($ads as $item) {
-                $like = Like::where('ads_id', '=', $item->id)->get();
-                $isLike = Like::where([
-                    ['ads_id', '=', $item->id],
-                    ['user_id', '=', $currentUser->id]
-                ])->count() > 0;
-                $comment = Comment::where('ads_id', '=', $item->id)->orderBy('created_at', 'desc')->paginate(Constant::NUM_OF_PAGE);
-                $comment_count = Comment::where('ads_id', '=', $item->id)->count();
-                $user = User::where('id', '=', $item->user_id)->get();
-
-                if (count($user) == 0) {
-                    $item->user = null;
-                } else {
-                    $item->user = $user[0];
-                }
-                $item->like = count($like);
-                $commentRes = [];
-                foreach ($comment->items() as $item2) {
-                    $commentUser = User::where('id', '=', $item2->user_id)->get();
-                    $item2->user = $commentUser[0];
-                    $commentRes[] = $item2;
-                }
-                $item->comment = $commentRes;
-                $item->isLike = $isLike;
-                $item->comment_count = $comment_count;
-                $isInFavorite = Favorite::where([
-                    ['ads_id', '=', $item->id],
-                    ['user_id', '=', $currentUser->id]
-                ])->count() > 0;
-                $item->isInFavorite = $isInFavorite;
-            }
-
-            return $this->get_response($ads->items(), 200, "completed");
-        }else if($request->type != null){
-                $ads = Ads::where('type' , $request->type)->orderBy('created_at', 'desc')->with('advantages')
-                ->with('images')
-                ->paginate(Constant::NUM_OF_PAGE)
-            ;
-            $currentUser = $request->user(); 
-            foreach ($ads as $item) {
-                $like = Like::where('ads_id', '=', $item->id)->get();
-                $isLike = Like::where([
-                    ['ads_id', '=', $item->id],
-                    ['user_id', '=', $currentUser->id]
-                ])->count() > 0;
-                $comment = Comment::where('ads_id', '=', $item->id)->orderBy('created_at', 'desc')->paginate(Constant::NUM_OF_PAGE);
-                $comment_count = Comment::where('ads_id', '=', $item->id)->count();
-                $user = User::where('id', '=', $item->user_id)->get();
-
-                if (count($user) == 0) {
-                    $item->user = null;
-                } else {
-                    $item->user = $user[0];
-                }
-                $item->like = count($like);
-                $commentRes = [];
-                foreach ($comment->items() as $item2) {
-                    $commentUser = User::where('id', '=', $item2->user_id)->get();
-                    $item2->user = $commentUser[0];
-                    $commentRes[] = $item2;
-                }
-                $item->comment = $commentRes;
-                $item->isLike = $isLike;
-                $item->comment_count = $comment_count;
-                $isInFavorite = Favorite::where([
-                    ['ads_id', '=', $item->id],
-                    ['user_id', '=', $currentUser->id]
-                ])->count() > 0;
-                $item->isInFavorite = $isInFavorite;
-            }
-
-            return $this->get_response($ads->items(), 200, "completed");
-        }else if($request->search_word != null){
-                $ads = Ads::where('name','like',$request->search_word)->orderBy('created_at', 'desc')->with('advantages')
-                ->with('images')
-                ->paginate(Constant::NUM_OF_PAGE)
-            ;
-            $currentUser = $request->user(); 
-            foreach ($ads as $item) {
-                $like = Like::where('ads_id', '=', $item->id)->get();
-                $isLike = Like::where([
-                    ['ads_id', '=', $item->id],
-                    ['user_id', '=', $currentUser->id]
-                ])->count() > 0;
-                $comment = Comment::where('ads_id', '=', $item->id)->orderBy('created_at', 'desc')->paginate(Constant::NUM_OF_PAGE);
-                $comment_count = Comment::where('ads_id', '=', $item->id)->count();
-                $user = User::where('id', '=', $item->user_id)->get();
-
-                if (count($user) == 0) {
-                    $item->user = null;
-                } else {
-                    $item->user = $user[0];
-                }
-                $item->like = count($like);
-                $commentRes = [];
-                foreach ($comment->items() as $item2) {
-                    $commentUser = User::where('id', '=', $item2->user_id)->get();
-                    $item2->user = $commentUser[0];
-                    $commentRes[] = $item2;
-                }
-                $item->comment = $commentRes;
-                $item->isLike = $isLike;
-                $item->comment_count = $comment_count;
-                $isInFavorite = Favorite::where([
-                    ['ads_id', '=', $item->id],
-                    ['user_id', '=', $currentUser->id]
-                ])->count() > 0;
-                $item->isInFavorite = $isInFavorite;
-            }
-
-            return $this->get_response($ads->items(), 200, "completed");
+    {
+        $searchField = [['status', Constant::ADS_ACCEPTED_STATE]];
+        if ($request->has('search_word') && !empty($request->search_word)) {
+            $searchField[] = ['name', 'LIKE', '%' . $request->search_word . '%'];
         }
-        return $this->get_error_response(401, "enter data to search for");
+
+        if ($request->has('type') && isset($request->type)) {
+            $searchField[] = ['type', $request->type];
+        }
+
+        $ads = Ads::where($searchField)->orderBy('created_at', 'desc')->with('advantages')
+            ->with('images')
+            ->paginate(Constant::NUM_OF_PAGE)
+        ;
+        $currentUser = $request->user();
+        foreach ($ads as $item) {
+            $like = Like::where('ads_id', '=', $item->id)->get();
+            $isLike = Like::where([
+                ['ads_id', '=', $item->id],
+                ['user_id', '=', $currentUser->id]
+            ])->count() > 0;
+            $comment = Comment::where('ads_id', '=', $item->id)->orderBy('created_at', 'desc')->paginate(Constant::NUM_OF_PAGE);
+            $comment_count = Comment::where('ads_id', '=', $item->id)->count();
+            $user = User::where('id', '=', $item->user_id)->get();
+
+            if (count($user) == 0) {
+                $item->user = null;
+            } else {
+                $item->user = $user[0];
+            }
+            $item->like = count($like);
+            $commentRes = [];
+            foreach ($comment->items() as $item2) {
+                $commentUser = User::where('id', '=', $item2->user_id)->get();
+                $item2->user = $commentUser[0];
+                $commentRes[] = $item2;
+            }
+            $item->comment = $commentRes;
+            $item->isLike = $isLike;
+            $item->comment_count = $comment_count;
+            $isInFavorite = Favorite::where([
+                ['ads_id', '=', $item->id],
+                ['user_id', '=', $currentUser->id]
+            ])->count() > 0;
+            $item->isInFavorite = $isInFavorite;
+        }
+
+        return $this->get_response($ads->items(), 200, "completed");
 
     }
 }
